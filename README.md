9fa488c8f4e08f824779023a628307b8b17c80db
aameer sala7i <abulameer@outlook.com>
2026-03-04 11:14:01 +0300

    Project structure setup successfully. ReactJs, @hello-pangea/dnd dev packages dependecies installed.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gran Project Manager Web App 
+## Kanban board built in typescript using react js and node js express
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>gran-proj-mngr  react js app</h2>
+    </>
+  )
+}
+
+export default App
